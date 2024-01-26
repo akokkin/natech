e07b840619d6f954f740750f204815f390c8411f
k00kis <akokkinn@gmail.com>
2024-01-26 17:00:14 +0200

    added tests descriptions and constants

--- playwright-e2e/src/login.spec.ts
+++ playwright-e2e/src/login.spec.ts
@@ -5,23 +5,27 @@ import LoginpageConstants from '../pages/constants/loginpage.constants.json';
 import HomepageConstants from '../pages/constants/homepage.constants.json';
 import Credentials from '../credentials.json';
 
-test('Negative Scenario - Login with incorrect password', async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.navigate();
+test.describe('Incorrect credentials login', () => {
+    test('should not allow me to enter home page.', async ({ page }) => {
+        const loginPage = new LoginPage(page);
+        await loginPage.navigate();
 
-    await page.fill(LoginpageConstants.usernameTextviewSelector, 'awrongusername');
-    await page.fill(LoginpageConstants.passwordTextviewSelector, 'awrongpassword00');
-    await loginPage.clickLoginButton();
-    const errorMessage = await page.textContent(LoginpageConstants.unsuccessfulLoginTextSelector);
-    expect(errorMessage).toContain(LoginpageConstants.unsuccessfulLoginText);
+        await page.fill(LoginpageConstants.usernameTextviewSelector, 'awrongusername');
+        await page.fill(LoginpageConstants.passwordTextviewSelector, 'awrongpassword00');
+        await loginPage.clickLoginButton();
+        const errorMessage = await page.textContent(LoginpageConstants.unsuccessfulLoginTextSelector);
+        expect(errorMessage).toContain(LoginpageConstants.unsuccessfulLoginText);
+    });
 });
 
-test('Positive Scenario - Login with correct credentials', async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.navigate();
+test.describe('Correct credentials login', () => {
+    test('should allow me to enter home page.', async ({ page }) => {
+        const loginPage = new LoginPage(page);
+        await loginPage.navigate();
 
-    await page.fill(LoginpageConstants.usernameTextviewSelector, Credentials.username);
-    await page.fill(LoginpageConstants.passwordTextviewSelector, Credentials.password);
-    await loginPage.clickLoginButton();
-    expect(page.url()).toContain(HomepageConstants.url);
+        await page.fill(LoginpageConstants.usernameTextviewSelector, Credentials.username);
+        await page.fill(LoginpageConstants.passwordTextviewSelector, Credentials.password);
+        await loginPage.clickLoginButton();
+        expect(page.url()).toContain(HomepageConstants.url);
+    });
 });
